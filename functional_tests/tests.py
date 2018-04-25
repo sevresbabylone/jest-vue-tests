3efffbed4141eda1af54199248fa30df828e1d7b
sevresbabylone <cheryl.francais@gmail.com>
2018-04-26 01:56:39 +0800

    updates test multiple users can start lists at different urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -57,7 +57,10 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
     def test_can_start_a_list_for_one_user(self):
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -82,6 +85,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
-        page_text = self.browser.find_element_by_tag_name('body')
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, cheryl_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
